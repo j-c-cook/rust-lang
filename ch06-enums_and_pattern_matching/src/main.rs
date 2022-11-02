f36af186bf23882a82a4264295ed29889f1d8944
j-c-cook <jack-c-cook@protonmail.com>
2022-11-01 21:11:18 -0500

    work with option

--- ch06-enums_and_pattern_matching/src/main.rs
+++ ch06-enums_and_pattern_matching/src/main.rs
@@ -25,13 +25,24 @@ fn main() {
     let _four = IpAddrKind::V4;
     let _six = IpAddrKind::V6;
 
-    let home = IpAddrKind::V4(127, 0, 0, 1);
-    let loopback = IpAddrKind::V6(String::from("::1"));
+    let _home = IpAddrKind::V4(127, 0, 0, 1);
+    let _loopback = IpAddrKind::V6(String::from("::1"));
 
     let m = Message::Write(String::from("hello"));
     m.call();
 
     // The option enum and its advantage over null values
-    // https://doc.rust-lang.org/book/ch06-01-defining-an-enum.html#the-option-enum-and-its-advantages-over-null-values
+    let _some_number = Some(5);
+    let _some_char = Some('e');
 
+    let _absent_number: Option<i32> = None;
+
+    let _x: i8 = 5;
+    let _y: Option<i8> = Some(5);
+    // let sum = _x + _y;  // Breaks
+
+    // Option API documentation: https://doc.rust-lang.org/std/option/enum.Option.html
+
+    // The match control flow construct
+    // TODO: continue here -> https://doc.rust-lang.org/book/ch06-02-match.html#the-match-control-flow-construct
 }
