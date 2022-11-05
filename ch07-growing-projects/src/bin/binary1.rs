fe65fac16a1fa2891b9d44a817ddd0db8a88fffd
j-c-cook <jack.cook@protonmail.com>
2022-11-04 21:36:17 -0500

    Starting on ch07

---
+++ ch07-growing-projects/src/bin/binary1.rs
@@ -0,0 +1,5 @@
+// cargo build --release
+
+fn main() {
+    println!("A binary file that is included in the release.")
+}
---
+++ ch07-growing-projects/src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    println!("Hello, world! From the main binary.");
+
+    // TODO: continue here: https://doc.rust-lang.org/book/ch07-02-defining-modules-to-control-scope-and-privacy.html#modules-cheat-sheet
+}
