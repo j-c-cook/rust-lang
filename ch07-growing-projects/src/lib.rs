5e8d0d6d8873708b2dbb159bf4242841b41a41ce
j-c-cook <jack.cook@protonmail.com>
2022-11-04 22:55:43 -0500

    Put note on where to continue in lib

--- ch07-growing-projects/src/lib.rs
+++ ch07-growing-projects/src/lib.rs
@@ -1,5 +1,7 @@
 // Library file
 
+// TODO: Continue here -> https://doc.rust-lang.org/book/ch07-03-paths-for-referring-to-an-item-in-the-module-tree.html#paths-for-referring-to-an-item-in-the-module-tree
+
 mod front_of_house {
     mod hosting {
         fn add_to_waitlist() {}
